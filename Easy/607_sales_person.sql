b6b4723f186f7dc9be15571e717a5c54c4ea63e2
Dhyuthi Sri Ramya <dhyuthiusa@gmail.com>
2025-07-28 16:57:37 -0500

    Updated 607 solution

--- Easy/607_sales_person.sql
+++ Easy/607_sales_person.sql
@@ -5,3 +5,12 @@
 -- Description: Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
 
 -- Solution:
+SELECT name
+FROM SalesPerson
+WHERE name NOT IN
+    (SELECT SalesPerson.name
+    FROM SalesPerson, Orders, Company
+    WHERE Company.name = 'RED'
+    AND SalesPerson.sales_id = Orders.sales_id
+    AND Company.com_id = Orders.com_id
+    )
