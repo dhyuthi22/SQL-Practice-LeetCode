e66b4bf7281a5e2e0c2fe3d2ed497705d789d692
Dhyuthi Sri Ramya <dhyuthiusa@gmail.com>
2025-07-31 17:38:24 -0500

    Updated solution

--- Easy/1068_product_sales_analysis_1.sql
+++ Easy/1068_product_sales_analysis_1.sql
@@ -5,3 +5,7 @@
 -- Description: Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
 
 -- Solution:
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
