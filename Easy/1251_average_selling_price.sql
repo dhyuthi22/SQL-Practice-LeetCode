45ccdcac4cb6d98a090b7cf5152c53ceaf352b2b
Dhyuthi Sri Ramya <dhyuthiusa@gmail.com>
2025-08-12 12:47:25 -0500

    updated 1251 solution

--- Easy/1251_average_selling_price.sql
+++ Easy/1251_average_selling_price.sql
@@ -5,9 +5,9 @@
 -- Description: Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places. If a product does not have any sold units, its average selling price is assumed to be 0.
 
 -- Solution:
-SELECT p.product_id, ROUND(SUM(u.units*p.price) / SUM(u.units),2) AS average_price
+SELECT p.product_id, ROUND(COALESCE(SUM(u.units*p.price) * 1.0 / NULLIF(SUM(u.units),0),0),2) AS average_price
 FROM Prices p
-JOIN UnitsSold u
+LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY product_id
